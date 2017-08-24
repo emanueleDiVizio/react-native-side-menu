09462cb92201173e6097ec02d4c5b49f355528d9
Emanuele <Emanuele.d>
2017-08-24 11:19:05 +0100

    try

--- index.js
+++ index.js
@@ -119,7 +119,7 @@ export default class SideMenu extends React.Component {
   onLayoutChange(e: Event) {
     const { width, height } = e.nativeEvent.layout;
     const openMenuOffset = width * this.state.openOffsetMenuPercentage;
-    const hiddenMenuOffset = width * this.state.hiddenMenuOffsetPercentage;
+    //const hiddenMenuOffset = width * this.state.hiddenMenuOffsetPercentage;
     this.setState({ width, height, openMenuOffset, hiddenMenuOffset });
   }
 
